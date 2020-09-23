77ee839e601efca20fc7b4a9213907ae1873f02e
EmalIsmail <43685206+EmalIsmail@users.noreply.github.com>
2020-09-24 03:27:17 +0430

    Update java_project_V6.0.java

--- java_project_V6.0.java
+++ java_project_V6.0.java
@@ -1,5 +1,6 @@
 import java.util.Scanner; // We use the Scanner class for getting input from the user
 class AddNumbers
+   // This comment is written by emal ismail.
 {
    public static void main(String args[])
    {
