c26e8edb338f1ace77c57a4e1a1a30e08af76eb3
Tesla47 <34379821+Tesla47@users.noreply.github.com>
2020-09-23 20:53:09 +0430

    java_project_V4.0.java
    This Version 4.0.
    Date: Sep 23, 2020
    Time: 8:52 pm

---
+++ java_project_V4.0.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; // We use the Scanner class for getting input from the user
+class AddNumbers
+{
+   public static void main(String args[])
+   {
+      int x, y, z;
+
+      System.out.println("Enter two integers to calculate their sum");
+      Scanner in = new Scanner(System.in);
+     
+      x = in.nextInt();
+      y = in.nextInt();
+      z = x + y;
+     
+      System.out.println("Sum of the integers = " + z);
+   }
+}
+
+// I didn't need the files that were on Version 3.0, So I deleted them.  
+
